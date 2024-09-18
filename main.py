bb30305e7a292f63c2457f586cd542beb01fc7fa
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-18 15:13:22 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import instaloader
+
+il = instaloader.Instaloader()
+
+username = "rustamovakromjon327"
+profile = instaloader.Profile.from_username(il.context, username=username)
+
+
+print(f"[+] Profile info: {username}\n{profile.biography}\nPosts count: {profile.mediacount}\nFollowers: {profile.followers}")
+
+
+for post in profile.get_posts():
+    print(post.url)
